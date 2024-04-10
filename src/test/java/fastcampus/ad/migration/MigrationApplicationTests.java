af39f1fe8172abd1e6656065f2cea9889e85e422
dongjoon1251 <joon1251@gmail.com>
2024-04-10 17:35:35 +0900

    CH04_03. 개발환경 구성 - 스프링 개발환경 설정

---
+++ src/test/java/fastcampus/ad/migration/MigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.ad.migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
