a4365d9eaf3a0da8707fba03611a6f5c7b90deea
dongjoon1251 <joon1251@gmail.com>
2024-04-10 17:36:19 +0900

    CH05_01. 레거시 DSP 모듈 추가

---
+++ legacy-dsp-application/src/main/java/fastcampus/ad/legacy/LegacyDspApplication.java
@@ -0,0 +1,12 @@
+package fastcampus.ad.legacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyDspApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LegacyDspApplication.class, args);
+  }
+}
