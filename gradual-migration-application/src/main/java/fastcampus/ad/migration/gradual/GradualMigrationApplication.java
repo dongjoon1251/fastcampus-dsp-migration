9b7940ed92ab35c04935afb1e2c24dabae6f6659
dongjoon1251 <joon1251@gmail.com>
2024-04-10 21:05:55 +0900

    CH07_01. 점진적 마이그레이션 애플리케이션 모듈 추가

---
+++ gradual-migration-application/src/main/java/fastcampus/ad/migration/gradual/GradualMigrationApplication.java
@@ -0,0 +1,12 @@
+package fastcampus.ad.migration.gradual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradualMigrationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GradualMigrationApplication.class, args);
+  }
+}
