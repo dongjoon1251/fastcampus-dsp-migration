5bb2c5288b008a0afd202ef7a785e9c7e28f2f5a
dongjoon1251 <joon1251@gmail.com>
2024-04-12 15:47:42 +0900

    CH11_07. 점진적 마이그레이션과 배치성 마이그레이션 동시에 시뮬레이션

--- legacy-dsp-application/src/test/java/fastcampus/ad/legacy/api/adgroup/LegacyAdGroupControllerTest.java
+++ legacy-dsp-application/src/test/java/fastcampus/ad/legacy/api/adgroup/LegacyAdGroupControllerTest.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 class LegacyAdGroupControllerTest {
@@ -45,6 +46,38 @@ class LegacyAdGroupControllerTest {
       }
     } catch (Exception e) {
       createAdGroup(count);
+    }
+  }
+
+  @Test
+  void update_many_adgroup() throws InterruptedException {
+    restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+    try (ExecutorService executorService = Executors.newFixedThreadPool(1000)) {
+      AtomicInteger count = new AtomicInteger(0);
+      int threadCount = 1000000 - count.get();
+      CountDownLatch latch = new CountDownLatch(threadCount);
+      for (int i = 0; i < threadCount; i++) {
+        executorService.execute(() -> {
+          updateAdGroup(count);
+          latch.countDown();
+        });
+      }
+      latch.await();
+    }
+  }
+
+  private void updateAdGroup(AtomicInteger count) {
+    try {
+      String name = "adgroup-" + (random.nextInt(1000) + 1);
+      Long id = random.nextLong(1000000) + 1L;
+      restTemplate.patchForObject("http://localhost:8080/adgroup/v1/name",
+          new LegacyAdGroupUpdateNameRequest(id, name), LegacyAdGroupResp.class);
+      if (count.incrementAndGet() % 100 == 0) {
+        System.out.println("progress: " + count + " at " + LocalDateTime.now());
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+//      updateAdGroup(count);
     }
   }
 }
